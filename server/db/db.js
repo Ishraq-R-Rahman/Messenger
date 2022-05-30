d986c33bdebab0bd4e20268ac3a9c6a7cb830c8f
Ishraq-R-Rahman <67277219+Ishraq-R-Rahman@users.noreply.github.com>
2022-05-31 02:32:19 +0600

    :sparkles:Implementing read status for messages

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,20 @@
 const Sequelize = require("sequelize");
 
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+//   {
+//     logging: false,
+//   }
+// );
+
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  "messenger", "shwarup","shwarup",
   {
+    host: "localhost",
+    dialect: "postgres",
     logging: false,
   }
 );
-
 
 
 module.exports = db;
